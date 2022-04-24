c5cf563a3c8e37b1b22ad102481782ff38bf8f9e
Amela <amelakalezic@gmail.com>
2022-04-24 11:10:14 -0500

    Adding maxDiff=None

--- test_module.py
+++ test_module.py
@@ -4,6 +4,7 @@ from budget import create_spend_chart
 
 
 class UnitTests(unittest.TestCase):
+    maxDiff=None
     def setUp(self):
         self.food = budget.Category("Food")
         self.entertainment = budget.Category("Entertainment")
